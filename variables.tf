510e03fb8892dbfc8b1d54245e14190c89b2844e
Terraform <romain.cambonie@gmail.com>
2023-04-18 10:15:21 +0200

    feat: adding terraform aws variables file

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "project" {
   nullable    = false
   description = "The name of the project that hosts the environment"
 }
+
+variable "service" {
+  type        = string
+  nullable    = false
+  description = "The name of the service that will be run on the environment"
+}
