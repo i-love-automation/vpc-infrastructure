9f5261ff79bbff74ca51c25d25af4dc35755b198
Romain Cambonie <romain.cambonie@gmail.com>
2023-04-18 15:16:16 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -15,3 +15,10 @@ variable "service" {
   nullable    = false
   description = "The name of the service that will be run on the environment"
 }
+
+variable "tfe_token" {
+  description = "TFE Team token"
+  nullable    = false
+  default     = false
+  sensitive   = true
+}
